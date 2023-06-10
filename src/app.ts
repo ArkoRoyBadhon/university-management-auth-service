906c99b31d1c86bed04b0bb1d7433c18c91305c0
Arko <arkoroyb@gmail.com>
2023-06-10 16:33:25 +0600

    semicolon added by a plugin

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,30 @@
-import express, { Application } from 'express'
-import cors from 'cors'
-import { UserRoutes } from './app/modules/users/user.route'
-import { logger } from './share/logger'
-import globalErrorHandler from './app/middlewares/globalErrorHandler'
+import express, { Application } from 'express';
+import cors from 'cors';
+import { UserRoutes } from './app/modules/users/user.route';
+import { logger } from './share/logger';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
 // import ApiError from './errors/ApiError'
 
-const app: Application = express()
+const app: Application = express();
 
-app.use(cors())
+app.use(cors());
 
 // parser
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Application routes
 
-logger.info(app.get('env'))
+logger.info(app.get('env'));
 
-app.use('/api/v1/users', UserRoutes)
+app.use('/api/v1/users', UserRoutes);
 
 // Testing
 app.get('/', async () => {
   // Promise.reject((new Error('Unhandled Promise rejection')))
-})
+});
 
 // global error handler
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
-export default app
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,46 +1,46 @@
 /* eslint-disable no-console */
-import mongoose from 'mongoose'
-import app from './app'
-import config from './config'
-import { logger, errorlogger } from './share/logger'
-import { Server } from 'http'
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
+import { logger, errorlogger } from './share/logger';
+import { Server } from 'http';
 
 process.on('uncaughtException', error => {
-  errorlogger.error(error)
-  process.exit(1)
-})
+  errorlogger.error(error);
+  process.exit(1);
+});
 
-let server: Server
+let server: Server;
 
 async function boostrap() {
   try {
-    await mongoose.connect(config.database_url as string)
-    logger.info(`Database is connected successfully`)
+    await mongoose.connect(config.database_url as string);
+    logger.info(`Database is connected successfully`);
 
     server = app.listen(config.port, () => {
-      logger.info(`Application app listening on port ${config.port}`)
-    })
+      logger.info(`Application app listening on port ${config.port}`);
+    });
   } catch (error) {
-    errorlogger.error(`failed to connect database`, error)
+    errorlogger.error(`failed to connect database`, error);
   }
 
   process.on('unhandledRejection', error => {
     if (server) {
       server.close(() => {
-        errorlogger.error(error)
-        process.exit(1)
-      })
+        errorlogger.error(error);
+        process.exit(1);
+      });
     } else {
-      process.exit(1)
+      process.exit(1);
     }
-  })
+  });
 }
 
-boostrap()
+boostrap();
 
 process.on('SIGTERM', () => {
-  logger.info('SIGTERM is received')
+  logger.info('SIGTERM is received');
   if (server) {
-    server.close()
+    server.close();
   }
-})
+});
